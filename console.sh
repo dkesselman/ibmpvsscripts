7713b9b70b62ce973f0bb87d442c740140f9f0a9
Diego E. KESSELMAN <42894928+dkesselman@users.noreply.github.com>
2025-09-29 16:12:38 -0600

    Update console.sh

--- console.sh
+++ console.sh
@@ -23,9 +23,9 @@ source EXPORT_LPAR_functions.inc
 CLOUDLOGIN
 
 #Recuperando la URL de la consola#
-consoleurl=$( ibmcloud pi ins con get  $InstanceName --json|jq .consoleURL|tr -d '"')
+consoleurl=$( ibmcloud pi ins con get  "$InstanceName" --json|jq .consoleURL|tr -d '"')
 echo $consoleurl
-#Opening Microsoft Edge Browser (Linux)
+#Opening Microsoft Edge Browser (Linux) - You can change this value to work with MacOS, Windows and with different browsers.
 microsoft-edge-stable  $consoleurl  2>&1 &
 # nohup open -a "Microsoft Edge" --url $consoleurl &  # For MacOS
 exit 0
